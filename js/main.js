1057c9e9e21b71d682e56435cbe3d1d641f634c7
Gustavo Rivero <gusir22@gmail.com>
2025-02-04 12:19:26 -0500

    Animate Batman JumpCatt.png sprite

--- js/main.js
+++ js/main.js
@@ -10,12 +10,25 @@ const CAT_HEIGHT = 32 * SPRITE_RATIO;
 // Sprite set up
 const spriteImage = new Image();
 spriteImage.src = 'sprites/ALLCats/BatmanCatFree/JumpCattt.png';
+spriteMaxFrame =  13 - 1; // 13 is the number of frames in jumpcatt.png, the minus 1 is to prevent the blank frame to appear and cause the animation to blink
 const spriteWidth = 32;
 const spriteHeight = 32;
+let frameX = 0; // tracks the current frame to print from the sprite in the x axis
+let spriteFrame = 0;
+const staggerAnimation = 4;
 
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    ctx.drawImage(spriteImage, 0, 0, spriteWidth, spriteHeight, 100, 45, CAT_WIDTH, CAT_HEIGHT);
+    ctx.drawImage(spriteImage, frameX * spriteWidth, 0, spriteWidth, spriteHeight, 100, 45, CAT_WIDTH, CAT_HEIGHT);
+    if (spriteFrame % staggerAnimation == 0) {
+        if (frameX < spriteMaxFrame) {
+            frameX++; // move to next frame
+        } else {
+            frameX = 0; // reset to zero
+        }
+    }
+
+    spriteFrame++;
     requestAnimationFrame(animate);
 };
 
